1d3118273136bb620cb0e586f96c1cf5fee26e16
Fatima Zahra Oukmi <Fatimazahra.oukmi@gmail.com>
2026-08-17 00:03:41 +0100

    Clean root directory by removing misplaced android files

--- app/src/main/java/com/healthtrack/mobile/models/PageResponse.java
+++
@@ -1,9 +0,0 @@
-package com.healthtrack.mobile.models;
-import java.util.List;
-public class PageResponse<T> {
-    private List<T> content;
-    private int totalElements, totalPages, size, number;
-    public List<T> getContent()     { return content; }
-    public int getTotalElements()   { return totalElements; }
-    public int getTotalPages()      { return totalPages; }
-}
